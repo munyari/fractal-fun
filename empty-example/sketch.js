802bc43c31de21364a5e8c28fcbb49c53e5f25a8
Panashe M. Fundira <fundirap@gmail.com>
2016-05-27 11:56:55 -0400

    nice cirlces!

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,18 +1,20 @@
 function setup() {
   var container = document.getElementById("myContainer");
-  var myCanvas = createCanvas(500,500);
+  var myCanvas = createCanvas(640,360);
   myCanvas.parent(container);
 }
 
 function draw() {
   background('white');
-  drawCircle(width/2, height/2, height);
+  drawCircle(width/2, height/2, 200);
 }
 
 function drawCircle(x, y, radius) {
+  stroke(0);
+  noFill();
   ellipse(x, y, radius, radius);
   if (radius > 2) {
-    radius *= 0.75;
-    drawCircle(x, y, radius);
+    drawCircle(x + radius/2, y, radius/2);
+    drawCircle(x - radius/2, y, radius/2);
   }
 }
